1956cfbec5a423a73581d29b37cd48e88a6bfbfc
manhpro2515 <manhpro2515@gmail.com>
2026-02-07 14:11:40 +0700

    app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ try:
     genai.configure(api_key=api_key)
     
     # Cấu hình Model mặc định (Dùng bản Flash cho nhanh và ổn định)
-    model = genai.GenerativeModel('gemini-1.5-flash')
+    model = genai.GenerativeModel('gemini-2.5-flash')
 except Exception as e:
     st.error("⚠️ Lỗi hệ thống: Giáo viên chưa cấu hình API Key trong phần Secrets.")
     st.stop()
